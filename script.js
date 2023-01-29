bba764c8dec0e856b00adb06aa26fb527d3818cb
Francois389 <75416431+Francois389@users.noreply.github.com>
2023-01-29 18:04:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,116 @@
+/**
+ * Redirige l'utilisateur vers une page dans un nouvel onglet ou non.
+ * @param {Text} lien
+ * @param {boolean} nouveauOnglet 
+ */
+function RedirectionJavascript(lien,nouveauOnglet=false){
+    // Fonction permettant de renvoyer vers une autre page
+    if (nouveauOnglet) {
+        window.open(lien,'_blank')
+    } else {
+        document.location.href=lien; 
+    }
+}
+
+/**
+ * Cache un élément html
+ * 
+ * @param {HTMLElement} element
+ */
+function cacherElt(element) {
+    element.style.display = "none";
+}
+
+/**
+ * Affiche un élément html
+ * 
+ * @param {HTMLElement} element
+ */
+function afficherElt(element) {
+    element.style.display = "block";
+}
+
+/**
+ * Fonction utiliser sur les boutons de bootstrap.
+ * Si le bouton est bleu (btn-primary), il devient gris (btn-secondary)
+ * Si le bouton est gris (btn-secondary), il devient bleu (btn-primary)
+ * 
+ * @param {HTMLElement} element
+ */
+function toggleCouleurBtn(element) {
+    if (element.classList.contains("btn-primary")) {
+        element.classList.remove("btn-primary");
+        element.classList.add("btn-secondary");
+    } else {
+        element.classList.remove("btn-secondary");
+        element.classList.add("btn-primary");
+    }
+}
+
+/**
+ * Affiche un élément HTMLElement s'il est caché
+ * Cache un élément HTMLElement s'il est affiché
+ * 
+ * @param {HTMLElement} element
+ */
+function toggleAffichage(element) {
+    if (estAfficher(element)) {
+        cacherElt(element);
+    } else {
+        afficherElt(element);
+    }
+}
+
+/**
+ * On utilise fa-spin de fontawesome pour faire tourner un element HTMLElement
+ * 
+ * Si l'élément HTMLElement contient la classe fa-spin, on la supprime
+ * Sinon on l'ajoute
+ * 
+ * @param {HTMLElement} html
+ */
+function toggleSpin(html) {
+    if (html.classList.contains("fa-spin")) {
+        html.classList.remove("fa-spin");
+    } else {
+        html.classList.add("fa-spin");
+    }    
+}
+
+/**
+ * Retourne true si l'élément est affiché, false sinon
+ * 
+ * @param {HTMLElement} element
+ * @returns {boolean} true si l'élément est affiché, false sinon
+ */
+function estAfficher(element) {
+    return !estCacher(element)
+}
+
+/**
+ * Retourne true si l'élément est caché, false sinon
+ * 
+ * @param {HTMLElement} element
+ * @returns {boolean} true si l'élément est caché, false sinon
+ */
+function estCacher(element) {
+    return element.style.display === "none";
+}
+
+
+
+// Début du code js exécuté au chargement de la page
+document.addEventListener('DOMContentLoaded', function() {  // Attente que le DOM soit prêt
+    
+    console.log("Je démarre le code js");
+
+    // On récupère les éléments du DOM
+    let btn = document.getElementById("HohoSound");
+
+    // On ajoute un écouteur d'événement sur le bouton
+    btn.addEventListener("click", function() {
+        const HohoBasique = new Audio('sound/Hoho.mp3');
+        HohoBasique.play();
+    });
+
+}); // Fin de l'attente que le DOM soit prêt
