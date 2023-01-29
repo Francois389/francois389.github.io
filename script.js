db28ebdb7a5bd5b2943bdaaeb7fbc1851e8f8e2d
Francois389 <75416431+Francois389@users.noreply.github.com>
2023-01-29 19:47:52 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -104,13 +104,56 @@ document.addEventListener('DOMContentLoaded', function() {  // Attente que le DO
     
     console.log("Je démarre le code js");
 
+    let sonplus = false;
+    let effetSelectionne = "normal";
+    let HohoAJouer = new Audio('sound/Hoho.mp3');
+
     // On récupère les éléments du DOM
     let btn = document.getElementById("HohoSound");
+    let divEffet = document.getElementById("divEffet")
+    let selectionEffet = document.getElementById("selectionEffet")
+
+    cacherElt(divEffet);
 
     // On ajoute un écouteur d'événement sur le bouton
     btn.addEventListener("click", function() {
-        const HohoBasique = new Audio('sound/Hoho.mp3');
-        HohoBasique.play();
+        if (!sonplus) {
+            sonplus = true;
+            afficherElt(divEffet);
+        }
+        /* selectionne le bon fichier correspondant à l'effet choisie parmi:
+           - reverberation
+           - reverse
+           - aigu
+           - distortion
+           - grave
+           - normal
+         */
+        switch (effetSelectionne) {
+            case "normal":
+                HohoAJouer = new Audio('sound/Hoho.mp3');
+                break;
+            case "reverberation":
+                HohoAJouer = new Audio('sound/HohoReverb.mp3');
+                break;
+            case "reverse":
+                HohoAJouer = new Audio('sound/HohoReverse.mp3');
+                break;
+            case "aigu":
+                HohoAJouer = new Audio('sound/HohoAigu.mp3');
+                break;
+            case "distortion":
+                HohoAJouer = new Audio('sound/HohoDistortion.mp3');
+                break;
+            case "grave":
+                HohoAJouer = new Audio('sound/HohoGrave.mp3');
+                break;
+        }
+        HohoAJouer.play();
+    });
+
+    selectionEffet.addEventListener("change", function() {
+        effetSelectionne = selectionEffet.value;
     });
 
 }); // Fin de l'attente que le DOM soit prêt
