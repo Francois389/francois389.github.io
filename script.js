b2fa2f35e79bf91ee18f216a2ece800510d367e6
Francois389 <75416431+Francois389@users.noreply.github.com>
2023-01-31 23:44:32 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -21,24 +21,37 @@ document.addEventListener('DOMContentLoaded', function() {  // Attente que le DO
     
     console.log("Je démarre le code js");
 
-    let sonplus = false;
+    let sonSuplementaire = false;
     let effetSelectionne = "normal";
     let HohoAJouer = new Audio('sound/Hoho.mp3');
+    let HohoLoop = false;
 
     // On récupère les éléments du DOM
-    let btn = document.getElementById("HohoSound");
-    let divEffet = document.getElementById("divEffet")
-    let selectionEffet = document.getElementById("selectionEffet")
+    const btn = document.getElementById("HohoSound");
+
+    //Bouton volume
+    const btnVolume = document.getElementById("boutonVolume");
+
+    //Liste effet
+    const divEffet = document.getElementById("divEffet");
+    const selectionEffet = document.getElementById("selectionEffet");
+
+    //Switch
+    const btnLoop = document.getElementById("boutonSwitch");
+    const divLoop = document.getElementById("divSwitch");
 
     cacherElt(divEffet);
 
+
     // On ajoute un écouteur d'événement sur le bouton
     btn.addEventListener("click", function() {
-        if (!sonplus) {
-            sonplus = true;
+        console.log("Principale Boucle : " + HohoLoop)
+        HohoAJouer.pause()
+        if (!sonSuplementaire) {
+            sonSuplementaire = true;
             afficherElt(divEffet);
         }
-        /* selectionne le bon fichier correspondant à l'effet choisie parmi:
+        /* sélectionne le bon fichier correspondant à l'effet choisi parmi :
            - reverberation
            - reverse
            - aigu
@@ -47,30 +60,45 @@ document.addEventListener('DOMContentLoaded', function() {  // Attente que le DO
            - normal
          */
         switch (effetSelectionne) {
-            case "normal":
-                HohoAJouer = new Audio('sound/Hoho.mp3');
-                break;
-            case "reverberation":
-                HohoAJouer = new Audio('sound/HohoReverb.mp3');
-                break;
-            case "reverse":
-                HohoAJouer = new Audio('sound/HohoReverse.mp3');
-                break;
-            case "aigu":
-                HohoAJouer = new Audio('sound/HohoAigu.mp3');
-                break;
-            case "distortion":
-                HohoAJouer = new Audio('sound/HohoDistortion.mp3');
-                break;
-            case "grave":
-                HohoAJouer = new Audio('sound/HohoGrave.mp3');
-                break;
+        case "normal":
+            HohoAJouer = new Audio('sound/Hoho.mp3');
+            break;
+        case "reverberation":
+            HohoAJouer = new Audio('sound/HohoReverb.mp3');
+            break;
+        case "reverse":
+            HohoAJouer = new Audio('sound/HohoReverse.mp3');
+            break;
+        case "aigu":
+            HohoAJouer = new Audio('sound/HohoAigu.mp3');
+            break;
+        case "distortion":
+            HohoAJouer = new Audio('sound/HohoDistortion.mp3');
+            break;
+        case "grave":
+            HohoAJouer = new Audio('sound/HohoGrave.mp3');
+            break;
         }
-        HohoAJouer.play();
+        HohoAJouer.loop = HohoLoop;
+        console.log("Btn Boucle : " + HohoAJouer.loop);
+        HohoAJouer.play().then(() => console.log("Son joué"));
+
     });
 
     selectionEffet.addEventListener("change", function() {
         effetSelectionne = selectionEffet.value;
+    });
+
+    divLoop.addEventListener("click", function() {
+        if (!HohoLoop) {
+            HohoLoop = true;
+        } else {
+            HohoLoop = false;
+            HohoAJouer.pause();
+        }
+        divLoop.classList.toggle("switchOn");
+
+        console.log("Switch Boucle : " + HohoLoop)
     });
 
 }); // Fin de l'attente que le DOM soit prêt
